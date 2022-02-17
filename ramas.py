8fe77d8fa37f9e0ca959a5badcd8b845b2936cc8
samuel mancera <samuel@gmail.com>
2022-02-17 14:16:21 -0500

    Primer codigo a las 2 y 16  pm

--- ramas.py
+++ ramas.py
@@ -1,9 +1,20 @@
 sensorNivel= ("Medio")
+sensorTemperatura= 16
 
-
+# eL PROGRAMA DE nIVEL DE AGUA
+"""
 if sensorNivel == ("Alto"):
     print("Esta bien de agua")
 if sensorNivel == ("Medio"):
     print("Precaución el agua se agota")
 if sensorNivel == ("Bajo"):
-    print("Peligro no hay agua")
+    print("Peligro no hay agua")"""
+    
+# El programa de temperatura
+
+if sensorTemperatura == 16:
+    print("aLERTA , LA TEMPERATURA SE SUBIO SE VA A ENCENDER EL VENTILADOR")
+if sensorTemperatura == 15:
+    print("LA TEMPERATURA ESTA OK")
+if sensorTemperatura == 14:
+    print("aLERTA LA TEMPERATURA ESTA BAJANDO , SE VA A PRENDER LA RESISTENCIA")
