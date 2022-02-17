53a6fbd1bfdf1938a31989d098ed5452db9cb0d0
samuel mancera <samuel@gmail.com>
2022-02-17 14:04:10 -0500

    Primer codigo a las 2 y 4 pm

---
+++ ramas.py
@@ -0,0 +1,9 @@
+sensorNivel= ("Medio")
+
+
+if sensorNivel == ("Alto"):
+    print("Esta bien de agua")
+if sensorNivel == ("Medio"):
+    print("Precaución el agua se agota")
+if sensorNivel == ("Bajo"):
+    print("Peligro no hay agua")
