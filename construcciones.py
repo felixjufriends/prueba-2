b324e6ff93ee904193d1e4697a51519a5a9704d5
samuel mancera <samuel@gmail.com>
2022-02-17 16:00:32 -0500

    Segundo codigo

--- construcciones.py
+++ construcciones.py
@@ -8,5 +8,12 @@ if Motor1 == ("sube"):
     print("esta arriba")
 if Motor1 == ("baja"):
     print("esta abajo")
-    
+#Programa Motor2
+
+if Motor2 == 1:
+    print("esta abierto")
+if Motor2 == 0:
+    print("esta cerrado")
+
+  
 
